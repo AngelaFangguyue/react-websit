5ac9a500ed505ab52ca0792f22bc7709f1ba3633
AngelaFang <1823476439@qq.com>
2020-09-20 16:58:21 +0800

    生成build目录

---
+++ precache-manifest.e55070b7fae4a8b96eec263435a22f50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930071d2dd77dc60f4e6e534c048bb4f",
+    "url": "/react-websit/index.html"
+  },
+  {
+    "revision": "9b2a0fb83afee3a40097",
+    "url": "/react-websit/static/css/main.4e46103c.chunk.css"
+  },
+  {
+    "revision": "b3f3eb4fd840d1952983",
+    "url": "/react-websit/static/js/2.1bf93838.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/react-websit/static/js/2.1bf93838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2a0fb83afee3a40097",
+    "url": "/react-websit/static/js/main.ea884a40.chunk.js"
+  },
+  {
+    "revision": "5a01e0fbfb0f8dca07cf",
+    "url": "/react-websit/static/js/runtime-main.76135ace.js"
+  }
+]);
